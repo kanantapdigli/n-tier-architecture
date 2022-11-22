91940dda544ddd06e21a6b7066439c133eb57365
KananTapdigli <kanan.tapdigli@gmail.com>
2022-11-23 00:08:13 +0400

    initial

---
+++ Web/Services/Abstract/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities;
+using Web.ViewModels.Category;
+
+namespace Web.Services.Abstract
+{
+    public interface ICategoryService
+    {
+        Task<CategoryIndexVM> GetAllAsync();
+        Task<CategoryUpdateVM> GetAsync(int id); 
+        Task CreateAsync(CategoryCreateVM model);
+        Task UpdateAsync(CategoryUpdateVM model);
+        Task DeleteAsync(int id);
+    }
+}
